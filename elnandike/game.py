7726a8af977a93969b4c71710db367c5b67ddc64
Elias Dorneles <eliasdorneles@gmail.com>
2017-04-19 18:26:51 -0400

    add methods implementing game logic moves

--- elnandike/game.py
+++ elnandike/game.py
@@ -52,6 +52,16 @@ class Deck(object):
         random.shuffle(self._cards)
 
 
+def suit_color(suit):
+    return 'red' if suit in ('diamonds', 'hearts') else 'black'
+
+
+def rank_diff(first, second):
+    """Return the relative difference between the given ranks"""
+    assert first in Deck.ranks and second in Deck.ranks
+    return Deck.ranks.index(second) - Deck.ranks.index(first)
+
+
 class Game(object):
     def __init__(self):
         deck = Deck()
@@ -70,6 +80,68 @@ class Game(object):
         if not self.stock:
             raise InvalidMove("No cards in stock")
         self.waste.append(self.stock.pop())
+        self.waste[-1].face_up = True
 
-    def move_pile(self, source_pile, target_pile):
-        assert source_pile in range(1, 8) and target_pile in range(1, 8)
+    def restore_stock(self):
+        self.stock = list(reversed(self.waste))
+        for card in self.stock:
+            card.face_up = False
+        self.waste.clear()
+
+    def _is_valid_move_to_tableau(self, source_card, target_card):
+        if not source_card.face_up or not target_card.face_up:
+            return False
+        diff = rank_diff(source_card.rank, target_card.rank)
+        return diff == 1 and suit_color(source_card.suit) != suit_color(target_card.suit)
+
+    def move_from_waste_to_tableau(self, target_index):
+        assert target_index in range(7)
+        target_pile = self.tableau[target_index]
+        if self.waste and self._is_valid_move_to_tableau(self.waste[-1], target_pile[-1]):
+            target_pile.append(self.waste.pop())
+        raise InvalidMove()
+
+    def move_tableau_pile(self, src_index, target_index):
+        """Move pile, assuming that cards facing up are in the proper order"""
+        assert src_index in range(7) and target_index in range(7)
+        if src_index == target_index:
+            raise InvalidMove()
+        source_pile, target_pile = self.tableau[src_index], self.tableau[target_index]
+        target_card = target_pile[-1]
+        for index, card in list(enumerate(source_pile))[::-1]:
+            if self._is_valid_move_to_tableau(card, target_card):
+                to_move = source_pile[index:]
+                target_pile.extend(to_move)
+                for _ in range(len(to_move)):
+                    source_pile.pop()
+                return
+        raise InvalidMove()
+
+    def _find_foundation_pile(self, card_to_move):
+        for pile in self.foundations:
+            if any([
+                    not pile and card_to_move.rank == 'A',
+                    pile and card_to_move.suit == pile[-1].suit and
+                    rank_diff(card_to_move.rank, pile[-1].rank) == -1
+            ]):
+                return pile
+
+    def move_to_foundation_from_waste(self):
+        if not self.waste:
+            raise InvalidMove()
+        foundation_pile = self._find_foundation_pile(self.waste[-1])
+        if foundation_pile is None:
+            raise InvalidMove()
+        foundation_pile.append(self.waste.pop())
+
+    def move_to_foundation_from_tableau(self, index):
+        assert index in range(7)
+        pile = self.tableau[index]
+        card_to_move = pile[-1]
+        if not card_to_move.face_up:
+            raise InvalidMove()
+
+        foundation_pile = self._find_foundation_pile(card_to_move)
+        if foundation_pile is None:
+            raise InvalidMove()
+        foundation_pile.append(pile.pop())
