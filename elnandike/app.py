ad1dd66f99113264b678a308914b3be7833c14a9
Elias Dorneles <eliasdorneles@gmail.com>
2017-04-19 20:51:27 -0400

    [WIP] refactor extracting GameApp class

--- elnandike/app.py
+++ elnandike/app.py
@@ -12,60 +12,65 @@ def exit_on_q(key):
         raise urwid.ExitMainLoop()
 
 
-def main(args):
-    game = Game()
-    statusbar = urwid.Text(u'Ready')
+class GameApp(object):
+    def __init__(self):
+        self.game = Game()
+        statusbar = urwid.Text(u'Ready')
 
-    def update_status(text):
-        statusbar.set_text(text)
+        def update_status(text):
+            statusbar.set_text(text)
 
-    def onclick(card_widget):
-        # XXX: code below is meant just for testing the UI,
-        # it's not related to actual game logic
-        card_widget.face_up = not card_widget.face_up
-        card_widget.highlighted = not card_widget.highlighted
+        def onclick(card_widget):
+            # XXX: code below is meant just for testing the UI,
+            # it's not related to actual game logic
+            card_widget.face_up = not card_widget.face_up
+            card_widget.highlighted = not card_widget.highlighted
 
-        update_status(
-            'Clicked: {}\nCard: {}\nHighlighted: {}'.format(
-                card_widget,
-                card_widget.card,
-                card_widget.highlighted,
+            update_status(
+                'Clicked: {}\nCard: {}\nHighlighted: {}'.format(
+                    card_widget,
+                    card_widget.card,
+                    card_widget.highlighted,
+                )
             )
-        )
-        card_widget.redraw()
+            card_widget.redraw()
 
-    def pile_card_clicked(card_widget, pile=None):
-        if not pile.top.face_up:
-            pile.top.face_up = True
-        else:
-            pile.cards.pop()
-            pile.redraw()
+        def pile_card_clicked(card_widget, pile=None):
+            if not pile.top.face_up:
+                pile.top.face_up = True
+            else:
+                pile.cards.pop()
+                pile.redraw()
 
-    main_layout = urwid.Pile([
-        urwid.Columns([
-            CardWidget(game.stock[-1]),
-            EmptyCardWidget(),
-            SpacerWidget(),
-            EmptyCardWidget(),
-            EmptyCardWidget(),
-            EmptyCardWidget(),
-            EmptyCardWidget(),
-        ]),
-        urwid.Divider(),
-        urwid.Columns([
-            CardPileWidget(pile, onclick=pile_card_clicked)
-            for pile in game.tableau
-        ]),
-        urwid.Divider(),
-        statusbar,
-    ])
+        self.main_layout = urwid.Pile([
+            urwid.Columns([
+                CardWidget(self.game.stock[-1]),
+                EmptyCardWidget(),
+                SpacerWidget(),
+                EmptyCardWidget(),
+                EmptyCardWidget(),
+                EmptyCardWidget(),
+                EmptyCardWidget(),
+            ]),
+            urwid.Divider(),
+            urwid.Columns([
+                CardPileWidget(pile, onclick=pile_card_clicked)
+                for pile in self.game.tableau
+            ]),
+            urwid.Divider(),
+            statusbar,
+        ])
 
+
+
+def main(args):
+    app = GameApp()
     if args.shell:
         from IPython import embed
         embed()
     else:
         loop = urwid.MainLoop(
-            urwid.Filler(main_layout, valign='top'),
+            urwid.Filler(app.main_layout, valign='top'),
             PALETTE,
             unhandled_input=exit_on_q,
         )
