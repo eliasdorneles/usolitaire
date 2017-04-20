e5b6067e78a94a165b825b0a2ac97a446c912901
Elias Dorneles <eliasdorneles@gmail.com>
2017-04-20 02:16:58 -0400

    some more refactoring

--- elnandike/app.py
+++ elnandike/app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Console-based Solitaire
+"""
 
 from __future__ import print_function, absolute_import, division
 import urwid
@@ -7,80 +9,63 @@ from .game import Game
 from .ui import CardWidget, CardPileWidget, SpacerWidget, EmptyCardWidget, PALETTE
 
 
+class GameApp(object):
+    def __init__(self):
+        self.game = Game()
+        self._statusbar = urwid.Text(u'Ready')
+        self._tableau_columns = urwid.Columns([
+            CardPileWidget(pile, onclick=self.pile_card_clicked)
+            for pile in self.game.tableau
+        ])
+        self._top_columns = urwid.Columns([
+            CardWidget(self.game.stock[-1]),
+            EmptyCardWidget(),
+            SpacerWidget(),
+            EmptyCardWidget(),
+            EmptyCardWidget(),
+            EmptyCardWidget(),
+            EmptyCardWidget(),
+        ])
+
+        self.main_layout = urwid.Pile([
+            self._top_columns,
+            urwid.Divider(),
+            self._tableau_columns,
+            urwid.Divider(),
+            self._statusbar,
+        ])
+
+    def pile_card_clicked(self, card_widget, pile=None):
+        if not pile.top.face_up:
+            pile.top.face_up = True
+            self.update_status('Good job')
+        else:
+            card = pile.cards.pop()
+            pile.redraw()
+            self.update_status('Popped: %r' % card)
+
+    def update_status(self, text):
+        self._statusbar.set_text(text)
+
+
 def exit_on_q(key):
     if key in ('q', 'Q', 'esc'):
         raise urwid.ExitMainLoop()
 
 
-class GameApp(object):
-    def __init__(self):
-        self.game = Game()
-        statusbar = urwid.Text(u'Ready')
-
-        def update_status(text):
-            statusbar.set_text(text)
-
-        def onclick(card_widget):
-            # XXX: code below is meant just for testing the UI,
-            # it's not related to actual game logic
-            card_widget.face_up = not card_widget.face_up
-            card_widget.highlighted = not card_widget.highlighted
-
-            update_status(
-                'Clicked: {}\nCard: {}\nHighlighted: {}'.format(
-                    card_widget,
-                    card_widget.card,
-                    card_widget.highlighted,
-                )
-            )
-            card_widget.redraw()
-
-        def pile_card_clicked(card_widget, pile=None):
-            if not pile.top.face_up:
-                pile.top.face_up = True
-            else:
-                pile.cards.pop()
-                pile.redraw()
-
-        self.main_layout = urwid.Pile([
-            urwid.Columns([
-                CardWidget(self.game.stock[-1]),
-                EmptyCardWidget(),
-                SpacerWidget(),
-                EmptyCardWidget(),
-                EmptyCardWidget(),
-                EmptyCardWidget(),
-                EmptyCardWidget(),
-            ]),
-            urwid.Divider(),
-            urwid.Columns([
-                CardPileWidget(pile, onclick=pile_card_clicked)
-                for pile in self.game.tableau
-            ]),
-            urwid.Divider(),
-            statusbar,
-        ])
-
-
-
 def main(args):
     app = GameApp()
-    if args.shell:
-        from IPython import embed
-        embed()
-    else:
-        loop = urwid.MainLoop(
-            urwid.Filler(app.main_layout, valign='top'),
-            PALETTE,
-            unhandled_input=exit_on_q,
-        )
-        loop.run()
+    loop = urwid.MainLoop(
+        urwid.Filler(app.main_layout, valign='top'),
+        PALETTE,
+        unhandled_input=exit_on_q,
+    )
+    loop.run()
 
 
 if '__main__' == __name__:
     import argparse
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--shell', action='store_true')
 
     args = parser.parse_args()
     main(args)
