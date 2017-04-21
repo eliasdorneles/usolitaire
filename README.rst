c63bfc02cc769d50c6bad200741a3a67e017c316
Elias Dorneles <eliasdorneles@gmail.com>
2017-04-21 12:23:56 -0400

    add README, setup tox and Travis

---
+++ README.rst
@@ -0,0 +1,24 @@
+===============================
+usolitaire
+===============================
+
+
+.. image:: https://img.shields.io/pypi/v/usolitaire.svg
+        :target: https://pypi.python.org/pypi/usolitaire
+
+.. image:: https://img.shields.io/travis/eliasdorneles/usolitaire.svg
+        :target: https://travis-ci.org/eliasdorneles/usolitaire
+
+.. image:: https://pyup.io/repos/github/eliasdorneles/usolitaire/shield.svg
+     :target: https://pyup.io/repos/github/eliasdorneles/usolitaire/
+     :alt: Updates
+
+
+Play solitaire in your terminal.
+
+Point and click to move cards.
+
+.. image:: screenshot-usolitaire.png
+
+
+* Free software: MIT license
--- setup.py
+++ setup.py
@@ -1,13 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from setuptools import setup
 
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    'urwid',
+]
 
 setup(
     name='usolitaire',
-    version='0.0.1',
-    packages=('usolitaire',),
+    version='0.1.0',
+    description="Solitaire in your terminal",
+    long_description=readme,
+    author="Elias Dorneles",
+    author_email='eliasdorneles@gmail.com',
+    url='https://github.com/eliasdorneles/usolitaire',
     entry_points={
         'console_scripts': {
             'usolitaire = usolitaire.app:main',
         }
     },
+    packages=[
+        'usolitaire',
+    ],
+    package_dir={'usolitaire':
+                 'usolitaire'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='usolitaire',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
