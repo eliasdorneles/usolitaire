3de4ac777dfb7b9e4417c922448cbb03f5bc28d9
Elias Dorneles <elias.dorneles@hey.com>
2023-07-27 14:53:21 +0200

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [
 
 setup(
     name="usolitaire",
-    version="1.0.0",
+    version="1.0.1",
     description="Solitaire in your terminal",
     long_description=readme,
     author="Elias Dorneles",
