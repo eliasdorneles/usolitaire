3266ff3ff237310e990daa3cf8536fe5d529932b
Dev Intern AI <dev-intern-ai@dev.ai>
2024-11-21 21:24:52 +0100

    In file usolitaire/__init__.py at line 10, Please remove these lines in __init__, they re not needed.

--- usolitaire/__init__.py
+++ usolitaire/__init__.py
@@ -4,7 +4,3 @@ __author__ = """Elias Dorneles"""
 __email__ = "eliasdorneles@gmail.com"
 __version__ = "1.1.0"
 
-from .game import Game
-from .card import Card
-from .deck import Deck
-from .exceptions import InvalidMove
