596c52680ebe82aa9fc1495561f32df80ba4d1a1
Dev Intern AI <dev-intern-ai@dev.ai>
2024-11-21 21:41:16 +0100

    In file usolitaire/utils.py at line 8, can we add the type hint in the utils functions as well?

--- usolitaire/utils.py
+++ usolitaire/utils.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from .deck import Deck
+from typing import Literal
 
-def suit_color(suit):
+def suit_color(suit: Literal["diamonds", "hearts", "clubs", "spades"]) -> Literal["red", "black"]:
     return "red" if suit in ("diamonds", "hearts") else "black"
 
-def rank_diff(first, second):
+def rank_diff(first: str, second: str) -> int:
     """Return the relative difference between the given ranks"""
     assert first in Deck.ranks and second in Deck.ranks
     return Deck.ranks.index(second) - Deck.ranks.index(first)
