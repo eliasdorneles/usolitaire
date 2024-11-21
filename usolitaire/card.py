c538264ae86c35522c87cfe358cf208693f4f746
Dev Intern AI <dev-intern-ai@dev.ai>
2024-11-21 21:28:09 +0100

    In file usolitaire/card.py at line 11, can you add type hints for these?

--- usolitaire/card.py
+++ usolitaire/card.py
@@ -8,10 +8,10 @@ SUIT_SYMBOLS = {
 }
 
 class Card(object):
-    def __init__(self, rank, suit, face_up=False):
-        self.rank = rank
-        self.suit = suit
-        self.face_up = face_up
+    def __init__(self, rank: str, suit: str, face_up: bool = False):
+        self.rank: str = rank
+        self.suit: str = suit
+        self.face_up: bool = face_up
 
     def __repr__(self):
         return "Card(rank={0.rank!r}, suit={0.suit!r}, face_up={0.face_up!r})".format(self)
